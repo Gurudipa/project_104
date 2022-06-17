2d227962d824bcd390f126b3d885db78c76518e1
Gurudipa <98025006+Gurudipa@users.noreply.github.com>
2022-06-17 22:34:05 +0530

    Add files via upload

---
+++ Login.1.js
@@ -0,0 +1,2 @@
+// Create login Function here.
+function Login();
---
+++ gamepage.js
@@ -0,0 +1,41 @@
+// random value generated
+  var y = Math.floor(Math.random() * 10 + 1);
+//  number of guesses
+var x = document.getElementById("guessField").value;
+// made for correct Guess
+if(x == y )
+{
+    alert("CONGRATULATUONIS !!!"+playername+" YOU GUESSED IT RIGHT IN "
+        + guess + "GUESS  ");
+        guess= 1;
+}
+  
+// function for number guessed by user   
+else if(x > y)
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+}
+else
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A GREATER NUMBER")
+}
+
+
+function playAgain(){
+    y = Math.floor(Math.random() * 10 + 1 );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
